94d2637952fb9f371b2b9f2ca0b48a625e847445
Camille Crapat <camille.capt@gmail.com>
2023-12-15 12:05:34 +0100

    correction bug : tri

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -38,7 +38,6 @@ export default class {
 				.bills()
 				.list()
 				.then((snapshot) => {
-					snapshot.sort((a, b) => (a.date < b.date ? 1 : -1));
 					const bills = snapshot.map((doc) => {
 						try {
 							return {
